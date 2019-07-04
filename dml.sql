be81f0ae32eded8c1eee8d49681e39e789c2ec4c
TangLijin <51473372+TangLijin@users.noreply.github.com>
2019-07-04 12:53:38 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+insert into student values('003','wangwu',22,'男');
 -- 修改记录  
-
+update student set age=19 where id='003';
 --  删除记录  
-
+delete from student where id='003';
 -- 查询记录  
+select * from student;
